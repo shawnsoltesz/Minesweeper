f1781e6bfb638b050247839b48f2978e791b613d
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-09-14 19:59:33 -0400

    reworking project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
-
 export function App() {
   const [game, setGame] = useState({
+    id: undefined,
     board: [
       [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
       [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
@@ -12,148 +12,111 @@ export function App() {
       [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
       [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
     ],
-    id: null,
-    winner: null,
+    state: undefined,
+    mines: undefined,
   })
 
-  async function handleClickCell(row: number, column: number) {
-    // console.log(`You clicked on row ${row} and column ${column}`)
-    console.log({ row, column })
+  async function newEasyGame() {
+    const gameOptions = { difficulty: 0 }
 
-    if (leftClick) {
-      // Generate the URL we need
-      const checkUrl = `https://minesweeper-api.herokuapp.com/games/${game.id}/check`
+    const url = 'https://minesweeper-api.herokuapp.com/games'
 
-      // Make an object to send as JSON
-      const body = { row, column }
-
-      // Make a POST request to make a move
-      const checkResponse = await fetch(checkUrl, {
-        method: 'POST',
-        headers: { 'content-type': 'application/json' },
-        body: JSON.stringify(body),
-      })
-
-      if (checkResponse.ok) {
-        // Get the response as JSON
-        const newGameState = await checkResponse.json()
-
-        // Make that the new state!
-        setGame(newGameState)
-      }
-    }
-    if (rightClick) {
-      // Generate the URL we need
-      const flagUrl = `https://minesweeper-api.herokuapp.com/games/${game.id}/flag`
-
-      // Make an object to send as JSON
-      const body = { row, column }
-
-      // Make a POST request to make a move
-      const flagResponse = await fetch(flagUrl, {
-        method: 'POST',
-        headers: { 'content-type': 'application/json' },
-        body: JSON.stringify(body),
-      })
-
-      if (flagResponse.ok) {
-        // Get the response as JSON
-        const newGameState = await flagResponse.json()
-
-        // Make that the new state!
-        setGame(newGameState)
-      }
-    }
-    async function handleNewGame() {
-      // Make a POST request to ask for a new game
-      const response = await fetch(
-        'https://minesweeper-api.herokuapp.com/games',
-        {
-          method: 'POST',
-          headers: { 'content-type': 'application/json' },
-        }
-      )
-
-      if (response.ok) {
-        // Get the response as JSON
-        const newGameState = await response.json()
-        setGame(newGameState)
-      }
+    const fetchOptions = {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(gameOptions),
     }
 
-    return (
-      <div>
-        <h1>
-          Minesweeper - <button>New</button>
-          Tic Tac Toe - {game.id} <button onClick={handleNewGame}>New</button>
-        </h1>
-        <ul>
-          <li onClick={() => handleClickCell(0, 0)}>{game.board[0][0]}</li>
-          <li onClick={() => handleClickCell(0, 1)}>{game.board[0][1]}</li>
-          <li onClick={() => handleClickCell(0, 2)}>{game.board[0][2]}</li>
-          <li onClick={() => handleClickCell(0, 3)}>{game.board[0][3]}</li>
-          <li onClick={() => handleClickCell(0, 4)}>{game.board[0][4]}</li>
-          <li onClick={() => handleClickCell(0, 5)}>{game.board[0][5]}</li>
-          <li onClick={() => handleClickCell(0, 6)}>{game.board[0][6]}</li>
-          <li onClick={() => handleClickCell(0, 7)}>{game.board[0][7]}</li>
-          <li onClick={() => handleClickCell(1, 0)}>{game.board[1][0]}</li>
-          <li onClick={() => handleClickCell(1, 1)}>{game.board[1][1]}</li>
-          <li onClick={() => handleClickCell(1, 2)}>{game.board[1][2]}</li>
-          <li onClick={() => handleClickCell(1, 3)}>{game.board[1][3]}</li>
-          <li onClick={() => handleClickCell(1, 4)}>{game.board[1][4]}</li>
-          <li onClick={() => handleClickCell(1, 5)}>{game.board[1][5]}</li>
-          <li onClick={() => handleClickCell(1, 6)}>{game.board[1][6]}</li>
-          <li onClick={() => handleClickCell(1, 7)}>{game.board[1][7]}</li>
-          <li onClick={() => handleClickCell(2, 0)}>{game.board[2][0]}</li>
-          <li onClick={() => handleClickCell(2, 1)}>{game.board[2][1]}</li>
-          <li onClick={() => handleClickCell(2, 2)}>{game.board[2][2]}</li>
-          <li onClick={() => handleClickCell(2, 3)}>{game.board[2][3]}</li>
-          <li onClick={() => handleClickCell(2, 4)}>{game.board[2][4]}</li>
-          <li onClick={() => handleClickCell(2, 5)}>{game.board[2][5]}</li>
-          <li onClick={() => handleClickCell(2, 6)}>{game.board[2][6]}</li>
-          <li onClick={() => handleClickCell(2, 7)}>{game.board[2][7]}</li>
-          <li onClick={() => handleClickCell(3, 0)}>{game.board[3][0]}</li>
-          <li onClick={() => handleClickCell(3, 1)}>{game.board[3][1]}</li>
-          <li onClick={() => handleClickCell(3, 2)}>{game.board[3][2]}</li>
-          <li onClick={() => handleClickCell(3, 3)}>{game.board[3][3]}</li>
-          <li onClick={() => handleClickCell(3, 4)}>{game.board[3][4]}</li>
-          <li onClick={() => handleClickCell(3, 5)}>{game.board[3][5]}</li>
-          <li onClick={() => handleClickCell(3, 6)}>{game.board[3][6]}</li>
-          <li onClick={() => handleClickCell(3, 7)}>{game.board[3][7]}</li>
-          <li onClick={() => handleClickCell(4, 0)}>{game.board[4][0]}</li>
-          <li onClick={() => handleClickCell(4, 1)}>{game.board[4][1]}</li>
-          <li onClick={() => handleClickCell(4, 2)}>{game.board[4][2]}</li>
-          <li onClick={() => handleClickCell(4, 3)}>{game.board[4][3]}</li>
-          <li onClick={() => handleClickCell(4, 4)}>{game.board[4][4]}</li>
-          <li onClick={() => handleClickCell(4, 5)}>{game.board[4][5]}</li>
-          <li onClick={() => handleClickCell(4, 6)}>{game.board[4][6]}</li>
-          <li onClick={() => handleClickCell(4, 7)}>{game.board[4][7]}</li>
-          <li onClick={() => handleClickCell(5, 0)}>{game.board[5][0]}</li>
-          <li onClick={() => handleClickCell(5, 1)}>{game.board[5][1]}</li>
-          <li onClick={() => handleClickCell(5, 2)}>{game.board[5][2]}</li>
-          <li onClick={() => handleClickCell(5, 3)}>{game.board[5][3]}</li>
-          <li onClick={() => handleClickCell(5, 4)}>{game.board[5][4]}</li>
-          <li onClick={() => handleClickCell(5, 5)}>{game.board[5][5]}</li>
-          <li onClick={() => handleClickCell(5, 6)}>{game.board[5][6]}</li>
-          <li onClick={() => handleClickCell(5, 7)}>{game.board[5][7]}</li>
-          <li onClick={() => handleClickCell(6, 0)}>{game.board[6][0]}</li>
-          <li onClick={() => handleClickCell(6, 1)}>{game.board[6][1]}</li>
-          <li onClick={() => handleClickCell(6, 2)}>{game.board[6][2]}</li>
-          <li onClick={() => handleClickCell(6, 3)}>{game.board[6][3]}</li>
-          <li onClick={() => handleClickCell(6, 4)}>{game.board[6][4]}</li>
-          <li onClick={() => handleClickCell(6, 5)}>{game.board[6][5]}</li>
-          <li onClick={() => handleClickCell(6, 6)}>{game.board[6][6]}</li>
-          <li onClick={() => handleClickCell(6, 7)}>{game.board[6][7]}</li>
-          <li onClick={() => handleClickCell(7, 0)}>{game.board[6][0]}</li>
-          <li onClick={() => handleClickCell(7, 1)}>{game.board[6][1]}</li>
-          <li onClick={() => handleClickCell(7, 2)}>{game.board[6][2]}</li>
-          <li onClick={() => handleClickCell(7, 3)}>{game.board[6][3]}</li>
-          <li onClick={() => handleClickCell(7, 4)}>{game.board[6][4]}</li>
-          <li onClick={() => handleClickCell(7, 5)}>{game.board[6][5]}</li>
-          <li onClick={() => handleClickCell(7, 6)}>{game.board[6][6]}</li>
-          <li onClick={() => handleClickCell(7, 7)}>{game.board[6][7]}</li>
-        </ul>
-      </div>
-    )
+    const response = await fetch(url, fetchOptions)
+
+    console.log(response)
+
+    if (response.ok) {
+      const newGameStateJson = await response.json()
+
+      setGame(newGameStateJson)
+    }
   }
+
+  async function handleClickCell(row: number, col: number) {
+    const checkOptions = {
+      id: game.id,
+      row,
+      col,
+    }
+
+    const url = `https://minesweeper-api.herokuapp.com/games/${game.id}/check`
+    const fetchOptions = {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(checkOptions),
+    }
+
+    const response = await fetch(url, fetchOptions)
+
+    if (response.ok) {
+      const newGameStateJson = await response.json()
+
+      setGame(newGameStateJson)
+    }
+  }
+
+  async function handleRightClickCell(row: number, col: number) {
+    const checkOptions = {
+      id: game.id,
+      row,
+      col,
+    }
+
+    const url = `https://minesweeper-api.herokuapp.com/games/${game.id}/flag`
+    const fetchOptions = {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(checkOptions),
+    }
+
+    const response = await fetch(url, fetchOptions)
+
+    if (response.ok) {
+      const newGameStateJson = await response.json()
+
+      setGame(newGameStateJson)
+    }
+  }
+
+  return (
+    <main>
+      <h1>Mine Sweeper</h1>
+      <h2>
+        <button onClick={newEasyGame}>New Easy Game</button>
+        <button>New Intermediate Game</button>
+        <button>New Difficult Game</button>
+      </h2>
+      <h3>Mines: {game.mines}</h3>
+      <h3>Game #: {game.id}</h3>
+
+      <section className="difficulty-0">
+        {game.board.map(function (gameRow, row) {
+          return gameRow.map(function (square, col) {
+            return (
+              <button
+                onClick={function (event) {
+                  event.preventDefault()
+                  handleClickCell(row, col)
+                }}
+                onContextMenu={function (event) {
+                  event.preventDefault()
+
+                  handleRightClickCell(row, col)
+                }}
+                key={col}
+              >
+                {square}
+              </button>
+            )
+          })
+        })}
+      </section>
+    </main>
+  )
 }
