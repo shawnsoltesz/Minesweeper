c81cbcb093abc9d74f94e2ff1016fef9573ac7bf
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-09-14 21:26:02 -0400

    updates .tsx and scss

--- src/App.tsx
+++ src/App.tsx
@@ -15,9 +15,9 @@ export function App() {
     state: undefined,
     mines: undefined,
   })
-
-  async function newEasyGame() {
-    const gameOptions = { difficulty: 0 }
+  const [difficulty, setDifficulty] = useState<0 | 1 | 2>(0)
+  async function newGame(newGameDifficulty: 0 | 1 | 2) {
+    const gameOptions = { difficulty: newGameDifficulty }
 
     const url = 'https://minesweeper-api.herokuapp.com/games'
 
@@ -34,6 +34,7 @@ export function App() {
     if (response.ok) {
       const newGameStateJson = await response.json()
 
+      setDifficulty(newGameDifficulty)
       setGame(newGameStateJson)
     }
   }
@@ -67,57 +68,52 @@ export function App() {
 
   function transformCellValue(value: string) {
     if (value === 'F') {
-      // return an icon for a flag
       return <i className="fa fa-flag" />
     }
 
     if (value === '_') {
-      // return an empty square
       return ' '
     }
 
     if (value === '*') {
-      // return an icon for a bomb
       return <i className="fa fa-bomb" />
     }
 
-    // otherwise, return what we have
     return value
   }
 
   function transformCellClassName(value: string | number) {
-    // return the appropriate className
-    if (value === 'F') {
-      return 'cell-flag'
-    }
+    switch (value) {
+      case 'F':
+        return 'cell-flag'
 
-    if (value === '*') {
-      return 'cell-bomb'
-    }
+      case '*':
+        return 'cell-bomb'
 
-    if (value === '_') {
-      return 'cell-free'
-    }
+      case '_':
+        return 'cell-free'
 
-    if ([1, 2, 3, 4, 5, 6, 7, 8].includes(Number(value))) {
-      return 'cell-number'
-    }
+      case ' ':
+        return undefined
 
-    return undefined
+      default:
+        // Must be a 1,2,3,etc
+        return `cell-number cell-${value}`
+    }
   }
 
   return (
     <main>
       <h1>Mine Sweeper</h1>
       <h2>
-        <button onClick={newEasyGame}>New Easy Game</button>
-        <button>New Intermediate Game</button>
-        <button>New Difficult Game</button>
+        <button onClick={() => newGame(0)}>New Easy Game</button>
+        <button onClick={() => newGame(1)}>New Intermediate Game</button>
+        <button onClick={() => newGame(2)}>New Difficult Game</button>
       </h2>
       <h3>Mines: {game.mines}</h3>
       <h3>Game #: {game.id}</h3>
 
-      <section className="difficulty-0">
+      <section className={`difficulty-${difficulty}`}>
         {game.board.map(function (gameRow, row) {
           return gameRow.map(function (square, col) {
             return (
